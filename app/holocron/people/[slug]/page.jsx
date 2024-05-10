54982bdf5a90875b77f93a180f5ad95f67dbaa8f
Lucascn21 <lucascn21@gmail.com>
2024-05-10 14:46:33 -0300

    add(-):create individual people and film pages

---
+++ app/holocron/people/[slug]/page.jsx
@@ -0,0 +1,13 @@
+"use server";
+import Section from "../../../_components/section";
+import { getData } from "../../../_lib/fetch";
+
+export default async function Page({ params }) {
+  const { slug } = params;
+  const data = await getData(`https://swapi.dev/api/people/${slug}`);
+  return (
+    <main>
+      <Section>{JSON.stringify(data)}</Section>
+    </main>
+  );
+}
