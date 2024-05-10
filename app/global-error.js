9e301bb8609ae0e1dd14de547167666305dcb0b1
Lucascn21 <lucascn21@gmail.com>
2024-05-10 13:16:44 -0300

    refactor(-):add return to home btn for error components

--- app/global-error.js
+++ app/global-error.js
@@ -1,11 +1,12 @@
 "use client";
-
+import Link from 'next/link'
 export default function GlobalError({ error, reset }) {
   return (
     <html>
       <body>
         <h2>Something went wrong!</h2>
         <button onClick={() => reset()}>Try again</button>
+        <Link href="/">Return Home</Link>
       </body>
     </html>
   );
